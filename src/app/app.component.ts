6698626a03997dee38de191ead856cb7ab4c2fda
mahajanmalay <malaymahajan.mm@gmail.com>
2022-06-30 15:04:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder,FormGroup,Validators, FormControl, MinLengthValidator} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment15';
+  constructor(public fbobj : FormBuilder){}
+
+  MyForm=this.fbobj.group(
+    {
+      firstName :['',[Validators.required,Validators.pattern("^[a-zA-Z._]+$")]],
+      lastName: ['',[Validators.required,Validators.pattern("^[a-zA-Z._]+$")]],
+      email: ['',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+      phone: ['',[Validators.required,Validators.pattern("[0-9]{10}")]],
+      city: ['',[Validators.required,Validators.minLength(4)]],
+      state: ['',],
+      zip:['',[Validators.required,Validators.pattern("[0-9]{5}")]],
+      comment: ['',Validators.maxLength(30)]
+
+    }
+  )
 }
